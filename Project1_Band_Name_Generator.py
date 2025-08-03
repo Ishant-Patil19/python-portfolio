d76ab2db614e410bb3016108eafc7789eb79d7c1
Ishant Patil <167984506+Ishant-Patil19@users.noreply.github.com>
2025-08-03 12:34:31 +0530

    Add files via upload

---
+++ Project1_Band_Name_Generator.py
@@ -0,0 +1,10 @@
+# Level: Beginner
+
+# Band Name Generator 🎸
+
+# A simple Python script that asks for your hometown and pet's name to generate a fun, personalized band name. Great for beginners learning input/output and string manipulation.
+
+print("Welcome to Band Name Generator")
+city_name = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+print("Your band name could be " + city_name + " " + pet_name)
