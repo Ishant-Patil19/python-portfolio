d3c3d1f503a2b85ff0783fd693ed733f3ea85652
Ishant Patil <AffilioXpress@hotmail.com>
2025-08-06 14:24:16 +0530

    New Program

---
+++ Project5_Password_Generator.py
@@ -0,0 +1,68 @@
+#Level: Beginner
+
+#PyPassword Generator 🔐
+
+"""
+A Python based password generator that creates secure, randomized passwords 
+with customizable counts of letters, numbers, and symbols. 
+Includes both a simple and a more secure version using list shuffling. 
+A perfect mini-project for practicing lists, loops, and the random module.
+
+"""
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print(
+
+"""
+  _____                                    _    _____                           _             
+ |  __ \                                  | |  / ____|                         | |            
+ | |__) |_ _ ___ _____      _____  _ __ __| | | |  __  ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+ |  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` | | | |_ |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+ | |  | (_| \__ \__ \\ V  V / (_) | | | (_| | | |__| |  __/ | | |  __/ | | (_| | || (_) | |   
+ |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|  \_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+                                                                                              
+"""
+)
+
+print("Welcome to PyPassword Generator")
+nr_letters = int(input("How many letters would you like in your password? \n"))
+nr_symbols = int(input(f"How many symbols would you like? \n"))
+nr_numbers = int(input(f"How many numbers would you like? \n"))
+
+#Easy Version:
+
+# password = ""
+# for char in range(0, nr_letters):
+#     password += random.choice(letters)
+
+# for char in range(0, nr_symbols):
+#     password += random.choice(symbols)
+
+# for char in range(0, nr_numbers):
+#     password += random.choice(numbers)
+
+# print(f"Your password is: {password}")
+
+#Hard Version:
+
+password_list = []
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for char in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
